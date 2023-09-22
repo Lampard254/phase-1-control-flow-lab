cea32ca6a038846c2c183d7b4475ed462c18cf62
Lampard254 <142304041+Lampard254@users.noreply.github.com>
2023-09-22 16:10:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
-function scuberGreetingForFeet() {
 
-    if (someNumber <= 400){
-            return "This one is on me!";
-    }
-    else if (someNumber > 2000 && someNumber < 2500){
-        return "I will gladly take your thirty bucks.";
-    }
-    else if(someNumber > 2500){
-        return "No can do.";
-    }
-
+// Write your code in this file!
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (2000 < feet && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity() {
-
-    if (city === "NYC") {
-        return "Ok, sounds good.";
-    }
-    else{
-        return "No go.";
-    }
+function ternaryCheckCity (city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-
-function switchOnCharmFromTip() {
-
-    if (tip === "generous") {
-        return "Thank you so much.";
-    } else if (tip === "not as generous") {
-        return "Thank you.";
-    } else {
-        return "Bye.";
-    }
-
-
-
-
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
